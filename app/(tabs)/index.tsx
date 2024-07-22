65a18cefb1e79155e15eaf9c3a5faf43ec6960d7
turangulcicek <turangulcicek0@gmail.com>
2024-07-22 15:50:57 +0300

    1. component completed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,9 @@
-import { StyleSheet } from "react-native";
-import Colors from "@/constants/Colors";
-import products from "@/assets/data/products";
-import { Image, FlatList, View, Text, ScrollView } from "react-native";
+import { View } from "react-native";
+import ProductListItem from "@/components/ProductListItem";
 export default function TabOneScreen() {
   return (
-    <ScrollView>
-      {products.map((pizza) => (
-        <View style={styles.innerContainer}>
-          <Image source={{ uri: pizza.image }} style={styles.image} />
-          <Text style={styles.text}>{pizza.name}</Text>
-          <Text style={styles.price}>{pizza.price}</Text>
-        </View>
-      ))}
-    </ScrollView>
+    <View>
+      <ProductListItem />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  innerContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 10,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-});
